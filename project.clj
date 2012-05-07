69fbf02e5ed1ef85d872680ca2d63965a53d015e
Emanuel Evans <emanuel.evans@gmail.com>
2012-05-07 19:28:19 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject msince45 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.6.0"]]
+  :plugins [[lein-swank "1.4.4"]])
