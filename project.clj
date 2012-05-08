43d1bf036950d1b2693f368e055c7b281049118d
Emanuel Evans <emanuel.evans@gmail.com>
2012-05-08 10:40:13 -0400

    Add basic sound event (still untested)

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.6.0"]]
-  :plugins [[lein-swank "1.4.4"]])
+  :dev-dependencies [[lein-swank "1.4.4"]
+                     [lein-marginalia "0.7.0"]])
--- src/msince45/sine-in-a.clj
+++ src/msince45/sine-in-a.clj
@@ -4,45 +4,32 @@
 (def harmonic-series-ratios
   (map inc (range 12)))
 
-(def default-vols
-  (vec (for [_ (range (count harmonic-series-ratios))] 1.0)))
+(def ^:dynamic *volumes*
+  (take 12 (iterate #(/ % 2) 1.0)))
 
-(defcgen sin-harmonics
-  [fundamental {:default 55.0}]
+(defcgen sin-sound
+  "A composite generator that takes as input a fundamental, a
+  duration, and a time step `t` and returns a stochastic sound event
+  with the volumes of harmonics determined by the `current-vols`
+  function."
+  [fundamental {:default 55.0}
+   duration    {:default 1.0}]
   (:ar
-   (let [freqs (map #(* fundamental %) harmonic-series-ratios)
-         vols (take 12 (iterate #(/ % 2) 1.0))]
-     (apply +
-            (map
-             (fn [freq vol]
-               (* (sin-osc freq) vol))
-             freqs vols)))))
+   (let [envelope (env-gen (sine duration))
+         frequencies (map #(* fundamental %) harmonic-series-ratios)]
+     (* envelope
+        (apply +
+               (map
+                (fn [freq vol]
+                  (* vol (sin-osc freq)))
+                frequencies *volumes*))))))
 
-(definst sin-event
-  [freq 55.0 vol 1.0]
-  (sin-harmonics))
-
-(defn cool-chord
-  []
-  (map sin-event
-       (map #(* 55.0 %) harmonic-series-ratios)
-       (take 12 (iterate #(/ % 2) 1.0))))
-
-(definst cool-inst
-  []
-  (apply +
-         (map #(sin-osc %)
-              [55 110 165 220 275 330 385 440 495 550 605 660])))
-
-(definst sin-inst
-  [freq 440 vol 1]
-  (* (sin-osc freq)
-     vol))
-
-(defn sins []
-  (map #(sin-inst %)
-       [55 110 165 220 275 330 385 440 495 550 605 660]))
-
-(definst two-freqs [freq1 220 freq2 440]
-  (apply +
-         (map #(sin-osc % 0) [freq1 freq2])))
+(defsynth sin-event
+  [fundamental 55.0
+   duration    1.0
+   volume      1.0
+   balance     0.5]
+  (out 0
+       (* volume
+          [balance (- 1 balance)]
+          (sin-sound fundamental duration))))
