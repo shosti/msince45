2b7ec61110ae80976d8278f19adc34d5b5429ebc
Emanuel Evans <emanuel.evans@gmail.com>
2012-05-08 19:21:37 -0400

    Got the synth and the basic runner working

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject msince45 "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject msince45 "1.0.0"
+  :description "Sine In A is a stochastic piece"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.6.0"]]
   :dev-dependencies [[lein-swank "1.4.4"]
-                     [lein-marginalia "0.7.0"]])
+                     [lein-marginalia "0.7.0"]]
+  :aot [:sine-in-a])
--- src/msince45/sine-in-a.clj
+++ src/msince45/sine-in-a.clj
@@ -4,32 +4,45 @@
 (def harmonic-series-ratios
   (map inc (range 12)))
 
-(def ^:dynamic *volumes*
+(def default-volumes
   (take 12 (iterate #(/ % 2) 1.0)))
 
-(defcgen sin-sound
-  "A composite generator that takes as input a fundamental, a
-  duration, and a time step `t` and returns a stochastic sound event
-  with the volumes of harmonics determined by the `current-vols`
-  function."
-  [fundamental {:default 55.0}
-   duration    {:default 1.0}]
+(defcgen sine-event
+  [fundamental
+   {:default 55 :doc "The fundamental frequency for the sine composite."}
+   duration
+   {:default 1.0 :doc "The duration of the sine composite, in seconds."}
+   volumes default-volumes
+   {:doc "A vector of the volumes for the respective overtones."}]
   (:ar
-   (let [envelope (env-gen (sine duration))
+   (let [envelope (env-gen (sine duration) :action FREE)
          frequencies (map #(* fundamental %) harmonic-series-ratios)]
      (* envelope
         (apply +
-               (map
-                (fn [freq vol]
-                  (* vol (sin-osc freq)))
-                frequencies *volumes*))))))
+               (map #(* %1 (sin-osc %2))
+                    volumes frequencies))))))
 
-(defsynth sin-event
-  [fundamental 55.0
-   duration    1.0
-   volume      1.0
-   balance     0.5]
-  (out 0
-       (* volume
-          [balance (- 1 balance)]
-          (sin-sound fundamental duration))))
+(defn sine-synth
+  [fundamental duration balance volumes]
+  (synth []
+         (out 0
+              (* [balance (- 1 balance)]
+                 (sine-event
+                  fundamental duration volumes)))))
+
+;;##Runner
+
+(def sine-metronome (metronome 300))
+
+(def all-freqs (cycle (map #(* 55.0 %) harmonic-series-ratios)))
+
+(defn play-sines
+  [beat freqs]
+  (let [next-beat (inc beat)
+        player (sine-synth (first freqs) 1 0.5 default-volumes)]
+    (at (sine-metronome beat)
+      (player))
+    (apply-at (sine-metronome next-beat) #'play-sines [next-beat (rest freqs)])))
+
+;(play-sines (sine-metronome) all-freqs)
+;(stop)
