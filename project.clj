ada3f22be6ff1e2d46fbd7981c792b19bf6c61eb
Emanuel Evans <emanuel.evans@gmail.com>
2012-05-09 02:20:29 -0400

    Created a play shell script.

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.6.0"]]
   :dev-dependencies [[lein-swank "1.4.4"]
-                     [lein-marginalia "0.7.0"]]
-  :main msince45.sine-in-a)
+                     [lein-marginalia "0.7.0"]])
