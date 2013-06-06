656b334038e70207ac5e9881694dbfd7b2e4a755
Emanuel Evans <emanuel.evans@gmail.com>
2013-06-06 11:41:31 -0700

    Project documentation

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject msince45 "1.0.0"
-  :description "Final project for \"Music Since 1945,\" taught by Katarina Miljkovic.  Completed 5/9/12."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [overtone "0.6.0"]]
-  :dev-dependencies [[lein-swank "1.4.4"]
-                     [lein-marginalia "0.7.0"]]
-  :marginalia {:javascript
-               ["http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default"]})
